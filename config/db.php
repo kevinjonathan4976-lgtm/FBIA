3cbc3a11d5eed9a57143471f272f89f85839b95a
MaxtecĦ <kevinjonathan4976@gmail.com>
2026-06-27 03:56:35 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,24 +1,17 @@
 <?php
+$host = "fdb1029.awardspace.net";
+$dbname = "4771289_fbia";
+$username = "4771289_fbia";
+$password = "FBIA@2026";
 
-$host = "localhost";
-$dbname = "fbia";
-$username = "root";
-$password = "";
-
-try{
-
-$conn = new PDO(
-"mysql:host=$host;dbname=$dbname;charset=utf8",
-$username,
-$password
-);
-
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}catch(PDOException $e){
-
-die("Database Connection Failed: ".$e->getMessage());
-
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
 }
-
 ?>
