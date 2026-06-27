f27a4fe7ded72624748e977572b7bea1a5cb9905
MaxtecĦ <kevinjonathan4976@gmail.com>
2026-06-27 00:54:44 -0700

    Create fbia.sql

---
+++ database/fbia.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE fbia;
+
+USE fbia;
+
+-- Users Table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user','admin') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Reports Table
+CREATE TABLE reports (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    report_id VARCHAR(30) UNIQUE NOT NULL,
+    fullname VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    anonymous BOOLEAN DEFAULT FALSE,
+    category VARCHAR(100),
+    location VARCHAR(255),
+    incident_date DATE,
+    incident_time TIME,
+    description TEXT,
+    evidence VARCHAR(255),
+    status ENUM(
+        'Received',
+        'Under Investigation',
+        'Resolved',
+        'Closed'
+    ) DEFAULT 'Received',
+    officer_comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Admin Table
+CREATE TABLE admins (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
+    password VARCHAR(255)
+);
